863f17ba2515b2b59417976aec8cf34fe373f030
Kyle Shang <you@example.com>
2018-08-29 17:28:44 -0400

    cleaned up project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './index.css';//applied global not like css class import
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
